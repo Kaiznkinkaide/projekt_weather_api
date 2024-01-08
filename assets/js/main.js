bf28997c0750ad1d2e1cd8066710c6f35e02842c
VictordalaM <delamotte.v@gamil.com>
2024-01-08 19:30:57 +0100

    abend

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,108 +1,79 @@
-let timeBox = document.getElementById('timeBox')
-let windBox = document.getElementById('windBox')
-let cloudsBox = document.getElementById("cloudsBox")
-let pressureBox = document.getElementById("pressureBox")
-let humitidyBox = document.getElementById("humitidyBox")
-let sunsetBox = document.getElementById("sunsetBox")
-let sunriseBox = document.getElementById("sunriseBox")
-let coordsBox = document.getElementById("coordsBox")
-let cityBox = document.getElementById('cityName')
-let nebeneinanderBox = document.getElementById('nebeneinanderBox')
-let countryCodeBox = document.getElementById('countryCodeBox')
-
-
-
 const check = () => {
-
+    clearInterval()
+    let input = document.getElementById('input').value
+    let cityOutput = document.getElementById('cityName')
+    let countryOutput = document.getElementById('countryCodeBox')
+    let weatherIconOutput = document.getElementById('icon')
+    let tempOutput = document.getElementById('temperatur')
     let timeOutput = document.getElementById('timeBox')
-    
-    const reset = () =>{
-        timeOutput.innerHTML = " "
+    let windOutput = document.getElementById('windBox')
+    let cloudsOutput = document.getElementById('cloudsBox')
+    let pressureOutput = document.getElementById('pressureBox')
+    let humitidyOutput = document.getElementById('humitidyBox')
+    let sunriseOutput = document.getElementById('sunriseBox')
+    let sunsetOutput = document.getElementById('sunsetBox')
+    let coordyOutput = document.getElementById('coordsBox')
+
+
+
+    const reset = () => {
+        weatherIconOutput.innerHTML = ""
+        cityOutput.innerHTML = ""
+        countryOutput.innerHTML = ""
+        tempOutput.innerHTML = ""
+        timeOutput.innerHTML = ""
+        windOutput.innerHTML = ""
+        cloudsOutput.innerHTML = ""
+        pressureOutput.innerHTML = ""  
+        humitidyOutput.innerHTML =""
+        sunriseOutput.innerHTML = ""
+        sunsetOutput.innerHTML = ""
+        coordyOutput.innerHTML = ""
     }
     reset()
 
-let input = document.getElementById('input').value
-console.log(input);
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=4bcea2e105b56117dc4f5cdd3a2391fd`)
 .then((response) => response.json())
 .then((data) => {
-    let currentdate = new Date()
-    console.log(currentdate);
-    let seconds = currentdate.getTime()
-    let time = new Date(seconds-3600000+data.timezone*1000).toLocaleTimeString()
-    let weatherIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let temp = Math.round(data.main.temp - 273.15) + "°"
-    let windSpeed = data.wind.speed
-    let clouds = data.weather[0].description
-    let pressure = data.main.pressure + " hpa"
-    let humitidy = data.main.humidity + " %"
-    let sunrise = new Date(data.sys.sunrise).toLocaleTimeString()
-    let sunset = new Date(data.sys.sunset).toLocaleTimeString()
-    let coordyX = data.coord.lat
-    let coordyY = data.coord.lon
-    let city = data.name
-    let country = data.sys.country
-
-    console.log(data);
-        let cityOutput = document.createElement('p')
-        cityOutput.innerHTML = " " + city
-        cityBox.appendChild(cityOutput)
-
-        let countryOutput = document.createElement('p')
-        countryOutput.textContent = country
-        countryCodeBox.appendChild(countryOutput)
-
-        let weatherIconOutput = document.createElement('img')
-        weatherIconOutput.setAttribute("src", weatherIcon) 
-        nebeneinanderBox.appendChild(weatherIconOutput)
-
-        let tempOutput = document.createElement('p')
-        tempOutput.textContent = temp
-        nebeneinanderBox.appendChild(tempOutput)
+        setInterval(() => {
+            let currentdate = new Date()
+            let seconds = currentdate.getTime()
+            let time = new Date(seconds-3600000+data.timezone*1000).toLocaleTimeString()
+            timeOutput.innerHTML = `<p>${time}</p>`
+        },1000)
+        // let currentdate = new Date()
+        // let seconds = currentdate.getTime()
+        // let time = new Date(seconds-3600000+data.timezone*1000).toLocaleTimeString()
+        let weatherIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        let temp = Math.round(data.main.temp - 273.15) + "°"
+        let windSpeed = `${data.wind.speed} (${data.wind.deg})`
+        let clouds = data.weather[0].description
+        let pressure = data.main.pressure + " hpa"
+        let humitidy = data.main.humidity + " %"
+        let sunrise = new Date(data.sys.sunrise).toLocaleTimeString()
+        let sunset = new Date(data.sys.sunset).toLocaleTimeString()
+        let coordyX = data.coord.lat
+        let coordyY = data.coord.lon
+        let city = data.name
+        let country = data.sys.country
 
 
-        // let timeOutput = document.createElement('p')
-        
-        timeOutput.innerHTML = `<p>${time}</p>`
-        timeBox.appendChild(timeOutput)
 
-        let windOutput = document.createElement('p')
-        windOutput.textContent = windSpeed
-        windBox.appendChild(windOutput)
+        weatherIconOutput.setAttribute("src", weatherIcon)
+        cityOutput.innerHTML = `<p>${city}</p>`
+        countryOutput.innerHTML = `<p>${country}</p>`
+        tempOutput.innerHTML = `<p>${temp}</p>`
+        // timeOutput.innerHTML = `<p>${time}</p>`
+        windOutput.innerHTML = `<p>${windSpeed}</p>`
+        cloudsOutput.innerHTML = `<p>${clouds}</p>`
+        pressureOutput.innerHTML = `<p>${pressure}</p>`    
+        humitidyOutput.innerHTML =`<p>${humitidy}</p>`
+        sunriseOutput.innerHTML = `<p>${sunrise}</p>`
+        sunsetOutput.innerHTML = `<p>${sunset}</p>`
+        coordyOutput.innerHTML = `<p>${coordyX}, ${coordyY}</p>`
 
-        let cloudsOutput = document.createElement('p')
-        cloudsOutput.textContent = clouds
-        cloudsBox.appendChild(cloudsOutput)
-
-        let pressureOutput = document.createElement('p')
-        pressureOutput.textContent = pressure
-        pressureBox.appendChild(pressureOutput)
-
-        let humitidyOutput = document.createElement('p')
-        humitidyOutput.textContent = humitidy
-        humitidyBox.appendChild(humitidyOutput)
-
-        let sunriseOutput = document.createElement('p')
-        sunriseOutput.textContent = sunrise
-        sunriseBox.appendChild(sunriseOutput)
-
-        let sunsetOutput = document.createElement('p')
-        sunsetOutput.textContent = sunset
-        sunsetBox.appendChild(sunsetOutput)
-
-        let coordyXOutput = document.createElement('p')
-        coordyXOutput.textContent = coordyX
-        coordsBox.appendChild(coordyXOutput)
-
-        let coordyYOutput = document.createElement('p')
-        coordyYOutput.textContent = coordyY
-        coordsBox.appendChild(coordyYOutput)
-
-        
-
-
-;
+        console.log( new Date(data.sys.sunrise));
+        console.log( new Date(data.sys.sunset));
 })
-
 }
